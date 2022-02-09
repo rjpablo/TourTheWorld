9c5d1182474bdccff7d6d6cbfaca6f39a41c7276
rjpablo <rjpablo.secondary@gmail.com>
2022-02-09 14:05:50 +0800

    Add project files.

---
+++ TourTheWorld/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TourTheWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
