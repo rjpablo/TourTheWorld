15de97b38715e11d99280769f18ed2c402690f4a
rjpablo <rjpablo.secondary@gmail.com>
2022-02-16 16:20:21 +0800

    feat: set default page to Tours/Index

--- TourTheWorld/Startup.cs
+++ TourTheWorld/Startup.cs
@@ -62,7 +62,7 @@ namespace TourTheWorld
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Tours}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
